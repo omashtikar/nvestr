250b79f65d08d415a07ba7cddcca0244afee6509
omashtikar <om.13.ashtikar@gmail.com>
2022-01-16 11:02:36 +0000

    .

--- market/admin.py
+++ market/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Company, CurrentMarketData, HistoricalMarketData
 
 # Register your models here.
+
+admin.site.register(Company)
+admin.site.register(CurrentMarketData)
+admin.site.register(HistoricalMarketData)
